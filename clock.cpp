3ed955175d4db9b06255cf95a6f113871f20c59d
Andrew Noyes <anoyes34@gmail.com>
2022-07-19 21:41:22 -0700

    Hours, seconds, minutes for clock

--- clock.cpp
+++ clock.cpp
@@ -3,16 +3,57 @@
 #include "draw_circle.hpp"
 #include "trig.hpp"
 
+constexpr auto clockRadius = 31;
+constexpr auto secondsRadius = 28;
+constexpr auto minutesRadius = 25;
+constexpr auto hoursRadius = 20;
+constexpr auto clockCenterX = 64;
+constexpr auto clockCenterY = 32;
+
+uint8_t seconds = 0;
+uint8_t minutes = 0;
+uint8_t hours = 0;
+
+void redraw() {
+  uint8_t secondsAngle = 256 / 4 - int(seconds) * 64 / 15;
+  uint8_t minutesAngle = 256 / 4 - int(minutes) * 64 / 15;
+  uint8_t hoursAngle = 256 / 4 - int(hours) * 64 / 12;
+  CLEAR();
+  draw_circle(clockCenterX, clockCenterY, clockRadius);
+  DRAW_LINE(clockCenterX, clockCenterY, 64 + cos(secondsAngle, secondsRadius),
+            32 - sin(secondsAngle, secondsRadius), 1);
+  DRAW_LINE(clockCenterX, clockCenterY, 64 + cos(minutesAngle, minutesRadius),
+            32 - sin(minutesAngle, minutesRadius), 1);
+  DRAW_LINE(clockCenterX, clockCenterY, 64 + cos(hoursAngle, hoursRadius),
+            32 - sin(hoursAngle, hoursRadius), 1);
+  DISPLAY();
+}
+
 int main() {
   api_init();
-  unsigned char angle = 0;
   CLEAR();
-  draw_circle(64, 32, 31);
+  uint8_t frames = 0;
+
+  redraw();
+
   for (;;) {
-    DRAW_LINE(64, 32, 64 + cos(angle, 30), 32 - sin(angle, 30), 0);
-    --angle;
-    DRAW_LINE(64, 32, 64 + cos(angle, 30), 32 - sin(angle, 30), 1);
-    DISPLAY();
+    ++frames;
+    if (frames == 20) {
+      frames = 0;
+      ++seconds;
+      if (seconds == 60) {
+        seconds = 0;
+        ++minutes;
+        if (minutes == 60) {
+          minutes = 0;
+          ++hours;
+          if (hours == 12) {
+            hours = 0;
+          }
+        }
+      }
+      redraw();
+    }
     WAIT();
   }
 }
