bd961a9a113239e3f10d71d9b86130d87b6f456a
Andrew Noyes <anoyes34@gmail.com>
2022-08-01 18:09:14 -0700

    Different implementations for different sized ints

--- string/string.cpp
+++ string/string.cpp
@@ -3,18 +3,7 @@
 namespace string {
 namespace detail {
 
-const char *itoa(uint32_t n) {
-  static char buf[11];
-  buf[10] = 0;
-  for (int i = 9; i >= 0; --i) {
-    buf[i] = '0' + (n % 10);
-    n /= 10;
-    if (!n) {
-      return buf + i;
-    }
-  }
-  return "?";
-}
+char itoaBuf[11];
 
 } // namespace detail
 } // namespace string
--- string/string.hpp
+++ string/string.hpp
@@ -9,7 +9,8 @@
 namespace string {
 
 namespace detail {
-const char *itoa(uint32_t n);
+uint8_t itoa(uint32_t n);
+extern char itoaBuf[11];
 
 template <int kLen> struct String {
   explicit constexpr String(const char *s) : s(s) {}
@@ -26,9 +27,8 @@ template <int kLen> constexpr String<kLen - 1> term(const char (&array)[kLen]) {
   return String<kLen - 1>{array};
 }
 
-template <bool kSigned,
-          class IntType = std::conditional_t<kSigned, int32_t, uint32_t>>
-struct Int {
+template <class IntType> struct Int {
+  static constexpr bool kSigned = std::is_signed_v<IntType>;
   explicit constexpr Int(IntType v) : v(v) {}
   static constexpr int max_length = 3 * sizeof(IntType) + (kSigned ? 1 : 0);
   void write(char *&buf) const {
@@ -39,9 +39,16 @@ struct Int {
         local = ~local + 1;
       }
     }
-    const char *formatted = itoa(local);
-    const uint8_t len = strlen(formatted);
-    memcpy(buf, formatted, len);
+    uint8_t len = 0;
+    for (int i = 10; i >= 0; --i) {
+      ++len;
+      itoaBuf[i] = '0' + (local % 10);
+      local /= 10;
+      if (!local) {
+        break;
+      }
+    }
+    memcpy(buf, itoaBuf + 11 - len, len);
     buf += len;
   }
 
@@ -50,8 +57,8 @@ private:
 };
 
 template <class IntType, class = std::enable_if_t<std::is_integral_v<IntType>>>
-constexpr Int<std::is_signed_v<IntType>> term(IntType s) {
-  return Int<std::is_signed_v<IntType>>{s};
+constexpr Int<IntType> term(IntType s) {
+  return Int<IntType>{s};
 }
 
 template <class L, class R> struct Concat {
